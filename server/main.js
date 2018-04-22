6603af7dc5a6bb72ec922d3d71cb49c91e0da864
Josh Hoeg <josh@revivedesignstudios.com>
2018-04-21 22:30:49 -0600

    todo app

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  render(<App />, document.getElementById('render-target'));
 });
